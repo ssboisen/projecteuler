af2f8ef376a78f078c67b9254fa8862c693a3fd4
Simon Skov Boisen <simon@skov-boisen.dk>
2012-02-07 11:40:43 +0100

    Solved problem 5

---
+++ src/problem_5.fsx
@@ -0,0 +1,12 @@
+﻿//See: http://en.wikipedia.org/wiki/Least_common_multiple and http://en.wikipedia.org/wiki/Greatest_common_divisor
+let smallestEvenlyDivisableBy seq = 
+        let rec gcd a b = if b = 0L then a else gcd b (a - b * (a / b))
+        let lcm a b = (a * b) / gcd a b
+        seq |> Seq.fold lcm 1L
+
+let answer = smallestEvenlyDivisableBy (seq { 1L..20L })
+
+
+
+
+
