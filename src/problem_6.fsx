51324769066b6509c88a504e34cdbeb348c2f562
Simon Skov Boisen <simon@skov-boisen.dk>
2012-02-07 12:02:05 +0100

    Solved problem 6

---
+++ src/problem_6.fsx
@@ -0,0 +1,7 @@
+﻿
+let problem6 =
+    let seq = seq { 1..100 }
+    let square = fun x -> x * x
+    let sumOfSquares = seq |> Seq.sumBy square
+    let squareOfSum = seq |> Seq.sum |> square
+    squareOfSum - sumOfSquares
