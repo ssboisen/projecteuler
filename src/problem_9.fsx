a7834eaeea0788f1aab7f2aff544645d6fc05d97
Simon Skov Boisen <simon@skov-boisen.dk>
2012-02-08 16:37:52 +0100

    Solved problem 9

---
+++ src/problem_9.fsx
@@ -0,0 +1,9 @@
+﻿#time;;
+let square x = x * x
+let answer = seq { for a in {1..1000} do
+                    for b in {a..1000} do
+                        let c = 1000 - a - b
+                        if square a + square b = square c then
+                            yield (a,b,c) } |> Seq.head
+
+answer |> fun (a,b,c) -> printfn "%A %d" (a,b,c) (a * b * c)
