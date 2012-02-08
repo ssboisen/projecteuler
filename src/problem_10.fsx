1aa323689a4675f1039858c0e86c7d3ccf66b0e7
Simon Skov Boisen <simon@skov-boisen.dk>
2012-02-08 18:29:59 +0100

    Solved problem 10

---
+++ src/problem_10.fsx
@@ -0,0 +1,12 @@
+﻿#r "FSharp.PowerPack.Parallel.Seq.dll"
+#time;;
+open Microsoft.FSharp.Collections
+open System
+
+let isPrime number =
+    let bound = int64(Math.Sqrt(float(number)))
+    seq { 2L .. bound }
+    |> Seq.exists (fun x -> number % x = 0L) |> not
+
+//Using PSeq cuts the real time in half
+let answer = {1L..2000000L} |> PSeq.filter isPrime |> PSeq.sum
