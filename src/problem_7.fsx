bdfd2f164f37da692deafa276354326aafb837b0
Simon Skov Boisen <simon@skov-boisen.dk>
2012-02-08 20:12:59 +0100

    Optimized the isPrime function a bit

--- src/problem_7.fsx
+++ src/problem_7.fsx
@@ -3,12 +3,15 @@
 open System
 
 let isPrime number =
-    let bound = int64(Math.Sqrt(float(number)))
-    seq { 2L .. bound }
-    |> Seq.exists (fun x -> if (number % x = 0L) then true else false)
-    |> not
+    match number with
+        | number when number = 2L -> true
+        | number when number <= 1L || number % 2L = 0L -> false
+        | _ ->
+                let bound = int64(Math.Sqrt(float(number)))
+                seq { 3L .. 2L .. bound }
+                |> Seq.exists (fun x -> number % x = 0L) |> not
 
-let result = Seq.initInfinite (fun index -> int64 index + 1L) |> Seq.filter isPrime |> Seq.nth 10000
+let result = Seq.initInfinite (fun index -> int64 index + 1L) |> Seq.filter isPrime |> Seq.nth 100000
 
 
 //Super fast sieve!
